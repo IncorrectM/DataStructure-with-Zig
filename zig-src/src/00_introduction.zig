8e8ac97a5f2ee7dcfbb93fb738f55047153ebb60
Zhihao Zhu <zzhao215@163.com>
2024-10-31 18:37:35 +0800

    :sparkle: chapter 00 introduction

--- zig-src/src/00_introduction.zig
+++ zig-src/src/00_introduction.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Hello DSwZ", .{});
+    std.debug.print("Hello DSwZ!\n", .{});
+    std.debug.print("使用zig build -Dchapter=1来开启第一章吧!\n", .{});
 }
