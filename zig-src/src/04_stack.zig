8bffeb3c7ea6134ab35a69d5d6b1d6ffe8e74594
IncorrectM <1287365321@qq.com>
2024-11-26 20:25:26 +0800

    :sparkles: basic framwork of stack

--- zig-src/src/04_stack.zig
+++ zig-src/src/04_stack.zig
@@ -1,5 +1,38 @@
 const std = @import("std");
 
-pub fn main() void {
+const ArrayList = @import("02_array.zig").SimpleArrayList;
+
+pub fn Stack(T: type) type {
+    return struct {
+        const This = @This();
+        const List = ArrayList(T);
+        allocator: std.mem.Allocator,
+        data: This.List,
+
+        pub fn init(allocator: std.mem.Allocator) !This {
+            return .{
+                .allocator = allocator,
+                .data = try This.List.init(allocator),
+            };
+        }
+
+        pub fn top(self: This) usize {
+            return self.data.len;
+        }
+
+        pub fn isEmpty(self: This) usize {
+            return self.top() == 0;
+        }
+
+        pub fn deinit(self: *This) void {
+            self.data.deinit();
+        }
+    };
+}
+
+pub fn main() !void {
     std.debug.print("Hello Stack!", .{});
+    var stack = try Stack(i32).init(std.heap.page_allocator);
+    std.debug.print("{}\n", .{stack.top()});
+    defer stack.deinit();
 }
